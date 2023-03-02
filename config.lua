a80ce47e139def039004aaccdf83cf8dda8b836f
6xDevelopment <126029462+6xDev@users.noreply.github.com>
2023-03-02 22:19:36 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,7 +10,18 @@ Config.TaskPedHash = 'a_m_y_stbla_02'
 Config.TaskPedLocation = vector3(-826.89, -690.01, 27.06)
 Config.TaskPedHeading = 88.75
 
-Config.BlipName = 'IT Comp. Call'
+Config.BlipName = 'IT Comp. Call' -- For calls
+Config.BlipName2 = 'IT Company'
+Config.BlipLocation = vector3(-827.46, -689.94, 28.06)
+Config.BlipName3 = 'IT Shop'
+Config.BlipLocation3 = vector3(88.99, -1101.38, 29.28)
+
+Config.CarSpawnCoord = vector4(-834.41, -685.18, 27.28, 0.29)
+Config.DeliveryVeh = "burrito3"
+Config.Fuel = "LegacyFuel"
+Config.VehPlate = "Delivery"
+local items = {itemmonitor, "6x_keyboard", "6x_mouse", "6x_compcase", "6x_powersupplie", "6x_cpu", "6x_cpucooler", "6x_motherboard", "6x_memory", "6x_graphiccard", "6x_ssd", "6x_cables"}
+Config.DeliveryItem = items[math.random(1, #items)]
 
 Config.Payout = math.random(2000, 4000)
 Config.Job = 'itcomp'
